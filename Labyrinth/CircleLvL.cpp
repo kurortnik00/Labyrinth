0fd0ae041144d1cbe8c5289d94baf1db46f76b3a
kurortnik00 <kurortnik00@gmail.com>
2019-10-29 02:05:29 +0300

    circleLvL remster

--- Labyrinth/CircleLvL.cpp
+++ Labyrinth/CircleLvL.cpp
@@ -6,27 +6,23 @@
 #define PI 3.14159265 
 
 CircleLvL::CircleLvL() 
-	:text("", font, 250),
-	_startPos(150,150),
+	:_startPos(150, 150),
 	_angVelocity(0.1),				//Rotation velocity
-	_velocity(0.1,0.1),				//Center velocity, it moves _center
-	_radius(400)			//_shape radiuse, it responsible of boundering shape. In fact it equils inscribed radius 
+	_velocity(0.1, 0.1),				//Center velocity, it moves _center
+	_radius(400),			//_shape radiuse, it responsible of boundering shape. In fact it equils inscribed radius 
+	line1(_center, 90, 2),
+	startButton(sf::Vector2f(300, 800), 50, "images/playButton.png", sf::IntRect(0, 0, 156, 156)),
+	winButton(sf::Vector2f(700, 200), 50, "images/winButton.png", sf::IntRect(0, 0, 126, 126))
 {
 	_position = sf::Vector2f(_startPos);
 	_angl = 0;
 	_center = sf::Vector2f(_position.x + _radius, _position.y + _radius);
 	_centerOfRotation = sf::Vector2f(_center.x - 100, _center.y - 100);			//dislocated center of rotation. made for shape movement 
-	_winShapeRadius = 10;								//the start shape of win animation 
-	_loseShapeRadius = 20;								//the shape that shows where you faild
-	_win = false;
-	gameResult = "You Lose!";
-	lastAniamation = false;		//flag that win animation has been done, followed by score screen
+
 	animationClock.restart();		//mabe it not necessaty
 	numberTeslaParticals = 18;
 	CircleLvL::Load("images/2/teslaColor");
-	font.loadFromFile("font/11583.ttf");
-	kinectControl = true;
-	//kinectApplication = Game::getKinectApplication();
+
 }
 
 CircleLvL::~CircleLvL() {
@@ -35,38 +31,22 @@ CircleLvL::~CircleLvL() {
 
 void CircleLvL::Load(std::string filename)
 {
-	//if (_image.loadFromFile(filename) == false) _isLoaded = false;
-	
-	finished = false;
-
-	///two boundering figure. They are not visible, they only for itaraction
 	_shape.setRadius(_radius);
 	_shape.setPosition(_position);
 
-	_line.setPosition(_center);
-	_line.setSize(sf::Vector2f(_radius, 10));
-	lineEnd = sf::Vector2f(_line.getPosition().x + _line.getSize().x, _line.getPosition().y);
+	Level::loadTextureArr(filename, 12, line1);
+	Level::setSpritesArr(line1, line1.animationTextureArr[0]);
+
+	line1._startPoint = _center;
+	lines.push_back(line1);
+
+	buttons.push_back(startButton);
+	buttons.push_back(winButton);
 
 
-	//two shapece for end of the game, second one when you lose, it shows where did you fail
-	//first one make a big bleaching of the screen
-	_winShape.setFillColor(sf::Color(255, 255, 255));
-	_winShape.setRadius(_winShapeRadius);
-	_loseShape.setFillColor(sf::Color(255, 0, 0));
-	_loseShape.setRadius(_loseShapeRadius);
-
-
-
-	loadTextureArr(filename, 12);	//loading all of the animations parts and save them in 'animationTextureArr_tesla', 12 images
-	animationNumber = 0;		//number of the current immage(freame)
-	setSpritesArr_circle(numberTeslaParticals, _texture);			//fill the sprites array  'spritesArr_teslaCircle' with the same _texture 
+	setSpritesArr_circle(numberTeslaParticals, line1.animationTextureArr[0]);			//fill the sprites array  'spritesArr_teslaCircle' with the same _texture 
 	for (int i = 0; i < spritesArr_teslaCircle.size(); i++) {
 		spritesArr_teslaCircle[i].setPosition(_position);			//init the start position of all sprites
-	}
-
-	setSpritesArr_line(2, _texture);	//fill the sprites array  'spriteArr_line' with the same _texture 
-	for (int i = 0; i < spriteArr_line.size(); i++) {
-		spriteArr_line[i].setPosition(_center - sf::Vector2f(5,45) + sf::Vector2f(210*i,0));		//init the start position of all sprites
 	}
 	_isLoaded = true;
 }
@@ -76,14 +56,13 @@ void CircleLvL::Draw(sf::RenderWindow & renderWindow)
 	//The main phase of game, while not win or lose
 	//while has been loaded and not finished it will draw all sprites from 'spriteArr_line' and 'spritesArr_teslaCircle'
 	//Buttons and timer draws in relevant classes
-	if (_isLoaded && !finished)
+	if (_isLoaded && !VisibleGameObject::getFinished())
 	{
 		for (int i = 0; i < spritesArr_teslaCircle.size(); i++) {
 			renderWindow.draw(spritesArr_teslaCircle[i]);		
 		}
-		for (int i = 0; i < spriteArr_line.size(); i++) {
-			renderWindow.draw(spriteArr_line[i]);
-		}
+		Level::drawLines(renderWindow, lines);
+		Level::drawButtons(renderWindow, buttons);
 	}
 	//drow the end of the game 
 	//last animation and score if win
@@ -91,54 +70,24 @@ void CircleLvL::Draw(sf::RenderWindow & renderWindow)
 	else
 	{
 
-		if (lastAniamation || !_win) {				//lastAnimation == true when plaer win
+		Level::win_lose_Draw(renderWindow, lines);
 
-			if (_win) {
-				std::ostringstream timerStr;
-				timerStr << gameResult;
-				text.setString(timerStr.str());
-				renderWindow.draw(text);
-				text.setPosition(400, 200);
-			}
-			else
-			{
-				//Lose state
-				//stop the moving animation 
-				//shows the faill place
-
-				for (int i = 0; i < spritesArr_teslaCircle.size(); i++) {
-					renderWindow.draw(spritesArr_teslaCircle[i]);
-				}
-				for (int i = 0; i < spriteArr_line.size(); i++) {
-					renderWindow.draw(spriteArr_line[i]);
-				}
-				renderWindow.draw(_loseShape);
-
-
-				//text that you failed
-				std::ostringstream timerStr;
-				timerStr << gameResult;
-				text.setString(timerStr.str());
-				renderWindow.draw(text);
-				text.setPosition(400, 200);
-			}
-			
-		}
-		else //when win proces comes here at first, then after a few moment it goes in win condition a litle bit higher
-		{
-			renderWindow.draw(_winShape);			//when win animation, 
-		}
 	}
 }
 
 void CircleLvL::Update(sf::Event& event) {
+
+
+	Level::lineAnimationUpdate(lines);
+	Level::linesUpdate(lines);
+	Level::buttonsUpdate(buttons);
 
 	animationTime = animationClock2.getElapsedTime().asMilliseconds();		//time for tesla animation
 	if (animationNumber == 12) animationNumber = 0;				//loop the animation
 	if (animationTime > 40) {								//the speed of animation
 		//int angleCircleTesla = 0;
 		for (int i = 0; i < spritesArr_teslaCircle.size(); i++) {
-			spritesArr_teslaCircle[i].setTexture(animationTextureArr_tesla[animationNumber]);
+			spritesArr_teslaCircle[i].setTexture(line1.animationTextureArr[animationNumber]);
 
 			double R = 390*1.2; //Радиус описанной окружности
 			double angle = 0; //Угол наклона правильного многоугольника
@@ -151,37 +100,29 @@ void CircleLvL::Update(sf::Event& event) {
 				
 			}	
 		}
-		//lop for KILL_line 
-		for (int i = 0; i < spriteArr_line.size(); i++) {
-			spriteArr_line[i].setTexture(animationTextureArr_tesla[animationNumber]);
-			spriteArr_line[i].setRotation(_angl);
-			spriteArr_line[i].setPosition(_center - sf::Vector2f(38*sin((_angl + 180)*PI/180) + 10*sin((_angl + 180 )*PI / 180), 38*cos(_angl*PI/180)) + sf::Vector2f(210 * i*cos(_angl*PI / 180), 210 * i*sin(_angl*PI / 180)));
-			//some kosteli and podgonian to make the rigtht ratation and make the same pace with bounding figure
-		}
-		//next animation image
+		
 		animationNumber++;		
 		animationClock2.restart();
 	}
 	
 	//this part runs when play button presed
 	//and runnig until not lost or win (finished)
-	if (!finished && VisibleGameObject::getStart())
+	if (!VisibleGameObject::getFinished() && VisibleGameObject::getStart())
 	{
 		animationTime_dinamic = animationClock.getElapsedTime().asMilliseconds();			//the time of velocity animation
 		//this animation rotates and move the KILL_ring
 		if (animationTime_dinamic > 5) {						//the animation speed
-			_angl += _angVelocity;
-			//transform.rotate(_angVelocity, _centerOfRotation);  //can be delited after make god lose screen
-			animationClock.restart();
 
+			_angl += _angVelocity;
 			_center = coordinateTransf(_angVelocity, _center, _centerOfRotation);		//canculation of new position of _center (the center of KILL_ring)
-			lineEnd = coordinateTransf(_angVelocity, lineEnd, _centerOfRotation);		//canculate the new pos of the line and citcle intersection point
 
 			//rotation of kill line 
 			//not visible, only for canculations of itaractions
-			_line.setRotation(_angl);
-			_line.setPosition(_center);
-
+			for (int j = 0; j < lines.size(); j++) {
+				lines[j]._startPoint = _center;
+				lines[j]._angl = _angl;
+			}
+			animationClock.restart();
 		}
 		if (_angl > 360) _angl -= 360;
 
@@ -201,150 +142,25 @@ void CircleLvL::Update(sf::Event& event) {
 		if (!VisibleGameObject::getKinectControll()) {
 			if ((dist2(sf::Vector2f(sf::Mouse::getPosition(Game::GetWindow()).x, sf::Mouse::getPosition(Game::GetWindow()).y), _center) > _radius*_radius))
 			{
-				losePoint(sf::Vector2f(sf::Mouse::getPosition(Game::GetWindow()).x, sf::Mouse::getPosition(Game::GetWindow()).y));
+				Level::lose(sf::Vector2f(sf::Mouse::getPosition(Game::GetWindow()).x, sf::Mouse::getPosition(Game::GetWindow()).y));
 			}
-			else if ((abs(lineEquation(_center, lineEnd, sf::Vector2f(sf::Mouse::getPosition(Game::GetWindow()).x, sf::Mouse::getPosition(Game::GetWindow()).y))) <= 2000)
-				&& (dist2(sf::Vector2f(sf::Mouse::getPosition(Game::GetWindow()).x, sf::Mouse::getPosition(Game::GetWindow()).y), lineEnd) < _radius*_radius))
-			{
-				losePoint(sf::Vector2f(sf::Mouse::getPosition(Game::GetWindow()).x, sf::Mouse::getPosition(Game::GetWindow()).y));
-			}
+			
 		}
-		else
-		{
-			for (int i = 0; i < JointType_Count; i++) {
-
-				joint_xy = sf::Vector2f(Game::getKinectApplication().SkeletPointsXY(i).x, Game::getKinectApplication().SkeletPointsXY(i).y);
-				joint_z = Game::getKinectApplication().DepthSkeletonPoints(i);
-
-				joint_xy.x = joint_xy.x * 1900 / 640 * 1 / 1; //translate to pixel
-				joint_xy.y = joint_xy.y * 1080 / 280 * 1 / 1;//same
-
-				if (joint_z >= _trashHold) {
-					if (animationClock.getElapsedTime().asMilliseconds() > 100) {						//need instad (event.type == sf::Event::MouseButtonPressed) to avoid mass click to target
-						if ((dist2(sf::Vector2f(joint_xy.x, joint_xy.y), _center) > _radius*_radius))
-						{
-							losePoint(sf::Vector2f(joint_xy.x, joint_xy.y));
-						}
-						else if ((abs(lineEquation(_center, lineEnd, sf::Vector2f(joint_xy.x, joint_xy.y))) <= 2000)
-							&& (dist2(sf::Vector2f(joint_xy.x, joint_xy.y), lineEnd) < _radius*_radius))
-						{
-							losePoint(sf::Vector2f(joint_xy.x, joint_xy.y));
-						}
-					}
-				}
-			}
-		}
+		
 	}
 
-	//Run win animation when screan circly go white
-	//In future better do with variable that depends from screeen values
-	else if (_win && (_winShapeRadius < 1500))												//Const of the animation PODGONIAN 
-	{
-		_winShapeRadius +=1.9;																//Const of the animation PODGONIAN 
-		_winShape.setRadius(_winShapeRadius);
-		_winShape.setPosition(_winShape.getPosition() - sf::Vector2f(1.6,1.6));				//Const of the animation PODGONIAN 
-	}
-	if (_winShapeRadius >= 1500 && _winShapeRadius <= 1600)									//when radiuse more then screeen 
-	{
-		lastAniamation = true;//finish of last animation
-		_winShapeRadius += 105;//some kosteli
-	}
 }
 
-//returns the sqwere of distance betvin two points
-//is used to canculate the iteraction with player
-float CircleLvL::dist2(sf::Vector2f const & p1, sf::Vector2f const & p2)
-{
-	return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
-}
-
-//returns new point after rotation around the point
-sf::Vector2f CircleLvL::coordinateTransf(float angl, sf::Vector2f & oldPoint, sf::Vector2f & centerOfRotation) {
-
-	float X = oldPoint.x - centerOfRotation.x;
-	float Y = oldPoint.y - centerOfRotation.y;
-
-	float NewCenter_X = X * cos(angl * PI / 180) - Y * sin(angl* PI / 180);
-	float NewCenter_Y = X * sin(angl * PI / 180) + Y * cos(angl* PI / 180);
-
-	return sf::Vector2f(NewCenter_X + centerOfRotation.x, NewCenter_Y + centerOfRotation.y); 
-}
-
-
-//equation for canculate coordinates of line when it rotate
-float CircleLvL::lineEquation(sf::Vector2f & point1, sf::Vector2f & point2, sf::Vector2f const & mouse) {
-	return (point1.y - point2.y)*mouse.x + (point2.x - point1.x)*mouse.y + (point1.x*point2.y - point2.x*point1.y);
-}
-
-void CircleLvL::win(sf::Vector2f pos) {
-	finished = true;
-	gameResult = "You Win!";
-	_winShape.setPosition(pos);
-	_win = true;
-}
-
-void CircleLvL::losePoint(sf::Vector2f pos) {
-	finished = true;
-	gameResult = "You Lose!";
-	//place the red shape on faid point (cordinate)
-	pos.x -= _loseShapeRadius;
-	pos.y -= _loseShapeRadius;
-	_loseShape.setPosition(pos);
-}
-
-//need for state mathine  
-bool CircleLvL::getFinished() {
-	return finished;
-}
 
 void CircleLvL::reInit() {
 
-	//back to init pos from rotation and moving
-	_position = sf::Vector2f(_startPos);
-	transform.rotate(360 - _angl, _centerOfRotation);
-	_angl = 0;
-	_line.setRotation(_angl);
-	_center = sf::Vector2f(_position.x + _radius, _position.y + _radius);
-	_winShapeRadius = 10;
-	animationClock.restart();
-	//set the reInition of boundering objcts posetions
-	_sprite.setPosition(_position);
-	_line.setPosition(_center);
-	lineEnd = sf::Vector2f(_line.getPosition().x + _line.getSize().x, _line.getPosition().y);
-
-	//reinit flag states
-	_win = false;
-	lastAniamation = false;
-	finished = false;
-	VisibleGameObject::setStart(false);
-	
-	//reInei the value of vin shape, that used in lastAnimation
-	_winShape.setRadius(_winShapeRadius);
-
-	animationNumber = 0;
 }
 
-void CircleLvL::loadTextureArr(std::string filename, int animationCount) {
-	for (int i = 1; i <= animationCount; i++) {
-		std::string s = filename + std::to_string(i) + ".png";
-		_image.loadFromFile(s);
-		_texture.loadFromImage(_image);
-	 	animationTextureArr_tesla.push_back(_texture);
-	}
-}
 
 void CircleLvL::setSpritesArr_circle(int figureCorners, sf::Texture texture)
 {
 	for (int i = 0; i < figureCorners; i++) {
 		_sprite.setTexture(texture);
 		spritesArr_teslaCircle.push_back(_sprite);
-	}
-}
-
-void CircleLvL::setSpritesArr_line(int lineCount, sf::Texture texture)
-{
-	for (int i = 0; i < lineCount; i++) {
-		_sprite.setTexture(texture);
-		spriteArr_line.push_back(_sprite);
 	}
 }
--- Labyrinth/circleLvL.h
+++ Labyrinth/circleLvL.h
@@ -3,8 +3,9 @@
 #include "VisibleGameObject.h"
 #include <sstream>
 #include "BodyBasics.h"
+#include "level.h"
 
-class CircleLvL : public VisibleGameObject
+class CircleLvL : public Level
 {
 public:
 	CircleLvL ();
@@ -12,61 +13,38 @@ public:
 	
 	void Draw(sf::RenderWindow & renderWindow);
 	void Update(sf::Event& event);
-	bool getFinished();
-	void win(sf::Vector2f pos);
-	void losePoint(sf::Vector2f pos);
 	void reInit();
 
 private:
 	sf::Sprite  _sprite;					//sprite of each one animation partical tesla
-	sf::Texture _texture;					//same for texture
-	sf::Image _image;						//--------------------
-	bool finished;							//flag of finish game set in win() and lose() functions
 	bool _isLoaded;							//set in Load()
 	void Load(std::string filename);		//use in Init() function 
-	sf::CircleShape _shape;					//use for boundering resulutions
-	sf::CircleShape _winShape;				//shape of win animation
-	float _winShapeRadius;					
-	sf::CircleShape _loseShape;				//shape that shows wher euou faild
-	float _loseShapeRadius;					
+	sf::CircleShape _shape;					//use for boundering resulutions				
 	float _radius;							//inscribed radius of mainCircle 
 	sf::Vector2f _position;					//pos of maiCircle left top corner
 	sf::Vector2f _velocity;					//velocity of center of rotation //not used
-	sf::RectangleShape _line;				//use for boundering resulutions
 	float _angl;							//angl of rotation
 	sf::Transform transform;				//function that rotate coordinate system
 	float _angVelocity;						//rotation velocity
 	sf::Clock animationClock;				//animation of moving
 	sf::Vector2f _centerOfRotation;			//pos of rotation center
 	sf::Vector2f _center;					//pos mainCircles center
-	float dist2(sf::Vector2f const & p1, sf::Vector2f const & p2);			//sqwere of dist betwin two points
-	sf::Vector2f coordinateTransf(float angl, sf::Vector2f & oldPoint, sf::Vector2f & centerOfRotation);		//returns new point after rotation around the point
-	sf::Vector2f lineEnd;	//end line point, use in lineEquasion() function
-	float lineEquation(sf::Vector2f & point1, sf::Vector2f & point2, sf::Vector2f const & mouse);		//function use for canculate collisions with line
-	bool  _win;		//flag set in win() function
-	sf::Font font;		//for timer and "win" or "lose" title
-	sf::Text text;		//for timer and "win" or "lose" title
-	std::string gameResult;	 //contains "win" or "lose" title
-	bool lastAniamation;				//flag is lastAnimation has been finished
 	sf::Vector2f _startPos;			//use in Init() and reInit()
-	std::vector< sf::Texture> animationTextureArr_tesla;			//vector of tetures that used in teslaCircle
-	void loadTextureArr(std::string filename, int animationCount);		//load textures and init animationTextureArr_tesla vector
 	int animationNumber;											//need for count what animation picture is now
 	std::vector< sf::Sprite> spritesArr_teslaCircle;				//vector of sprites that used in teslaCircle
 	void setSpritesArr_circle(int figureCorners, sf::Texture texture);		//init spritesArr_teslaCircle vector					
 	sf::Clock animationClock2;			//clock for animation speed of teslaCircle 
-	std::vector<sf::Sprite> spriteArr_line;		////vector of sprites that used in teslaLine
-	void setSpritesArr_line(int figureCorners, sf::Texture texture);	////set  spriteArr_line vector 
+	std::vector<sf::Sprite> spriteArr_line;		////vector of sprites that used in teslaLine 
 	float animationTime;		//contains count of miliSeconds from last sprite change (animation)
 	int numberTeslaParticals;		//number of particals(small lines) in mainCircle
 	float animationTime_dinamic;	////the time of velocity animation, contains  count of miliSeconds from last move (move animation)
 
 
-	bool kinectControl;		//if kinectControl == false than mouse control of game 
-	sf::Vector2f joint_xy;		//one of joints pos 
-	float joint_z;				//one of joints depth
-	float _trashHold;				//depth from sensor where interaction starts
-	//CBodyBasics kinectApplication;		//object that contains kinect functions
+	Line line1;
+	Button startButton;
+	Button winButton;
 
+	std::vector<Line> lines;
+	std::vector<Button> buttons;
 };
 
